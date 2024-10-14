50b249007b81c631915a3e5f273345d23460dff7
unknown <creepygolubkov@gmail.com>
2024-10-14 12:37:17 +0300

    Frontend progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
+const Filter = ({name}) => {
+  const [isSelected, setIsSelected] = useState(false)
+  return (
+    <button className='border border-slate-500 text-2xl rounded-md p-1 border-2' onClick={() => setIsSelected(!isSelected)}>
+      {name} {isSelected === true ? "❌" : ""}
+    </button>
+  )
+}
+
+const SearchResult = ({name, rating, setDetailedName}) => {
+  return (
+    <li key={name}><button onClick={() => {setDetailedName(name);console.log("clicked")}} className="bg-gray-700 text-7xl font-bold m-1">{name} {rating}⭐</button></li>
+  )
+}
 
 function App() {
+  const [jsonData, setJsonData] = useState(null)
+  const [detailedName, setDetailedName] = useState(null)
+
+  const getPubByIndex = (index) => {
+    return {
+      name: jsonData.name[index],
+      address: jsonData.address[index],
+      place_id: jsonData.place_id[index],
+      rating: jsonData.rating[index],
+      total_ratings: jsonData.total_ratings[index],
+      reviews: jsonData.reviews[index],
+    }
+  }
+
+  useEffect(() => {
+    fetch('../Helsinki_pubs_reviews.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.name[0]); // Access your JSON data here
+      setJsonData(data)
+      // console.log(Object.values(data.rating)[0])
+    })
+    .catch(error => {
+      console.error('Error loading JSON:', error);
+    });
+
+    // Set the tab name (document title) when the component mounts
+    document.title = "Pubfinder";
+    // Function to change the favicon dynamically
+    const changeFavicon = (faviconURL) => {
+      const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      link.href = faviconURL;
+      document.getElementsByTagName('head')[0].appendChild(link);
+    };
+
+    // Set the favicon when the component mounts
+    changeFavicon('../icons8-beer-48.png');
+  }, []);
+
+  const Detailed = ({name, setDetailedName}) => {
+    if (jsonData === null) {
+      console.log('jsondata null')
+      return
+    }
+    if (name === null) {
+      console.log('detailedname null')
+      return
+    }
+    const index = Object.entries(jsonData.name).filter(entry => entry[1] === name)[0][0]
+    // console.log(index)
+    const pub = getPubByIndex(index)
+  
+    return (
+      <div className='bg-gray-300 min-h-screen text-4xl p-2'>
+        <ul>
+          <li><h1 className='font-bold text-'>{pub.name} {pub.rating}⭐</h1></li>
+          <li><h3>Total reviews: {pub.total_ratings}</h3></li>
+          <li><h3>Text reviews analyzed by us: {pub.reviews.length}</h3></li>
+          <li><a className='hover:text-slate-700' target='_blank' href={'https://www.google.com/maps/place/?q=place_id:' + pub.place_id}>Address: {pub.address}</a></li>
+          <li><button onClick={() => setDetailedName(null)} className='border border-2 border-slate-500 rounded-md p-1 my-2'>Return</button></li>
+        </ul>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <div className='bg-gray-700 text-white text-4xl p-4 font-bold'>Topdeck</div>
-      <div className="bg-gray-300 text-white min-h-screen flex items-center justify-center">
-        <h1 className="bg-gray-700 text-7xl font-bold">Hello, Tailwind CSS in React!</h1>
+      <div className='bg-gray-700 text-white text-4xl p-4 font-bold'>Pubfinder🍺</div>
+      <div className={detailedName === null ? 'visible' : 'hidden'}>
+        <div className='bg-gray-400'>
+          <div className='p-4 flex text-2xl justify-evenly font-medium'>
+            Filters:
+            <Filter name="Beer🍺"></Filter>
+            <Filter name="Wine🍷"></Filter>
+            <Filter name="Cocktails🍸"></Filter>
+            <Filter name="Shots🥃"></Filter>
+            <Filter name="Dance💃"></Filter>
+            <Filter name="Rock🤘"></Filter>
+            <Filter name="Hip-hop😎"></Filter>
+            <Filter name="Pop🎵"></Filter>
+            <Filter name="Bar🥂"></Filter>
+            <Filter name="Club🪩"></Filter>
+            <Filter name="Pet-friendly🐕"></Filter>
+          </div>
+        </div>
+        <div className='bg-gray-300 text-3xl font-bold p-4'>Search results:</div>
+        <div className="bg-gray-300 text-white min-h-screen flex items-center justify-center">
+          <ul>
+            {jsonData === null ? "" : Object.keys(jsonData.name).map(index => getPubByIndex(index)).sort((a, b) => b.rating - a.rating).map(pub => <SearchResult name={pub.name} rating={pub.rating} key={pub.name} setDetailedName={setDetailedName}></SearchResult>)}
+          </ul>
+        </div>
       </div>
+      <Detailed name={detailedName} setDetailedName={setDetailedName}></Detailed>
     </div>
   );
 }
--- scraper.py
+++ scraper.py
@@ -9,80 +9,78 @@ from bs4 import BeautifulSoup
 import time
 
 # Initialize the Chrome browser with Selenium
-service = Service('./chromedriver')  # Update the path
-options = webdriver.ChromeOptions()
-# options.add_argument("--headless")  # Runs the browser in headless mode.
-driver = webdriver.Chrome(service=service, options=options)
+def scrapePage(place_id, passedDriver=None):
+    url = f"https://www.google.com/maps/place/?q=place_id:{place_id}"
+    time0 = time.time()
+    if passedDriver:
+        driver = passedDriver
+    else:
+        service = Service('chromedriver.exe')  # Update the path
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--headless")  # Runs the browser in headless mode.
+        options.add_argument('--lang=en-GB')
+        driver = webdriver.Chrome(service=service, options=options)
 
-# Open Google Maps URL (sample business)
 
-# <button class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 XWZjwc" jscontroller="soHxf" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue; touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc; touchcancel:JMtRjd; focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;mlnRJb:fLiPzd;" data-idom-class="nCP5yc AjY5Oe DuMIQc LQeN7 XWZjwc" jsname="tWT92d" aria-label="Reject all"><div class="VfPpkd-Jh9lGc"></div><div class="VfPpkd-J1Ukfc-LhBDec"></div><div class="VfPpkd-RLmnJb"></div><span jsname="V67aGc" class="VfPpkd-vQzf8d">Reject all</span></button>
+    # Open Google Maps URL (sample business)
 
-driver.get("https://www.google.com/maps/place/East-West+Pub/@60.1990582,24.9378081,17z/data=!4m8!3m7!1s0x469209901ebb1ad3:0xd6ab54bd134e1f1f!8m2!3d60.1990582!4d24.940383!9m1!1b1!16s%2Fg%2F11ckkyrnfx?entry=ttu&g_ep=EgoyMDI0MDkyNS4wIKXMDSoASAFQAw%3D%3D")
+    # <button class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 XWZjwc" jscontroller="soHxf" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue; touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc; touchcancel:JMtRjd; focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;mlnRJb:fLiPzd;" data-idom-class="nCP5yc AjY5Oe DuMIQc LQeN7 XWZjwc" jsname="tWT92d" aria-label="Reject all"><div class="VfPpkd-Jh9lGc"></div><div class="VfPpkd-J1Ukfc-LhBDec"></div><div class="VfPpkd-RLmnJb"></div><span jsname="V67aGc" class="VfPpkd-vQzf8d">Reject all</span></button>
 
-time.sleep(500)
-# Wait for the "Accept all" button to appear (you might need to adjust this depending on Google's structure)
-try:
-    # Wait until the cookies consent "Accept all" button is present and clickable
-    reject_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button//span[contains(text(), 'Accept all')]"))
-    )
-    reject_button.click()  # Click the "Accept all" button
-
-    print("Cookies consent rejected.")
-
-except Exception as e:
-    print("Error clicking the cookies button:", e)
-  
-# Wait for the reviews to load
-time.sleep(5)
-
-# body = driver.find_element(By.CLASS_NAME, "iwhWtc") # e07Vkf kA9KIf
-element = driver.find_element(By.CLASS_NAME, 'kA9KIf')
-actions = ActionChains(driver)
-
-# Move the cursor to the element
-# print(element.location)
-# actions.move_to_element(element).click().perform()
-time.sleep(2)
-element.send_keys(Keys.DOWN)
-time.sleep(2)
-# body = driver.find_element(By.TAG_NAME, "body")
-# body.send_keys(Keys.TAB)
-# time.sleep(2)
-# body = driver.find_element(By.TAG_NAME, "body")
-# body.send_keys(Keys.TAB)
-# time.sleep(2)
-# body.send_keys(Keys.TAB)
-# time.sleep(2)
-# Scroll down to load all reviews (scrolling multiple times to load more)
-for i in range(10):  # Adjust range as necessary to load more reviews
-    print('scroll')
-    driver.execute_script("window.scrollBy(0, 1000);")
-    body = driver.find_element(By.TAG_NAME, "body")
-    body.send_keys(Keys.ARROW_DOWN)
+    driver.get(url) # "https://www.google.com/maps/place/East-West+Pub/@60.1990582,24.9378081,17z/data=!4m8!3m7!1s0x469209901ebb1ad3:0xd6ab54bd134e1f1f!8m2!3d60.1990582!4d24.940383!9m1!1b1!16s%2Fg%2F11ckkyrnfx?entry=ttu&g_ep=EgoyMDI0MDkyNS4wIKXMDSoASAFQAw%3D%3D"
+    
+    
+    # Wait for the reviews to load
     time.sleep(2)
-
-# Extract page source and pass it to BeautifulSoup
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-# Find all the review elements (the exact tag/class might change)
-reviews = soup.find_all('div', class_='jftiEf fontBodyMedium')  # Update class as per actual HTML
-
-# Parse the reviews
-for review in reviews:
-    # Find review text
     try:
-      review_text = review.find('span', class_='wiI7pd').text
+        reviews = driver.find_element(By.XPATH, "//button//div//div[contains(text(), 'Reviews')]")
+    except:
+    #    print(f'Error {e}')
+       return []
+    reviews.click()
+    time.sleep(1)
+    actions = ActionChains(driver)
+    element = driver.find_element(By.CLASS_NAME, 'fontDisplayLarge') # wiI7pd
+    try:
+        element.click()
     except e:
-      print(f'error {e}')
-      continue
-    print("Review:", review_text)
+        print(f'Error: {e}')
+        return []
+    for _ in range(50):  # Adjust range as necessary to load more reviews
+        # print('scroll')
+        actions.send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(0.2)
+    # time.sleep(200)
+    
+    # Extract page source and pass it to BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # Find the rating
-    rating = review.find('span', class_='kvMYJc').text
-    print("Rating:", rating)
-    print("\n")
+    # Find all the review elements (the exact tag/class might change)
+    reviews = soup.find_all('div', class_='jftiEf fontBodyMedium')  # Update class as per actual HTML
 
-# Close the browser
-driver.quit()
+    # Parse the reviews
+    reviewtexts = []
+    for review in reviews:
+        # Find review text
+        try:
+          span = review.find('span', class_='wiI7pd')
+          if span:
+            review_text = span.text
+            reviewtexts.append(review_text)
+        except e:
+          print(f'error {e}')
+          continue
+        # print("Review:", review_text)
+
+        # Find the rating
+        # rating = review.find('span', class_='kvMYJc').text
+        # print("Rating:", rating)
+        # print("\n")
+    
+    # Close the browser
+    # driver.quit()
+    print('reviews scraped:', len(reviewtexts))
+    print('time elapsed:', time.time() - time0)
+    return reviewtexts
+
+if __name__ == '__main__':
+    scrapePage("ChIJV04qDAAJkkYRwltKh2f6YJ8")
